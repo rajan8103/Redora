4efce269565e2c2e201da3363020aae864e2f708
Rajan <rrai31721@gmail.com>
2025-08-27 19:14:04 +0530

    Fixed case-sensitive import issue

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URI);
-    console.log("mongodb  connected successfully");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully ✅");
   } catch (error) {
-    console.error("database conection failed:", error.message);
+    console.error("Database connection failed ❌:", error.message);
     process.exit(1);
   }
 };
